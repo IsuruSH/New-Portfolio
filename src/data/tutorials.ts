f8eb902ca174ad51241f53831ac2bb9ea597baf0
IsuruSH <shanakaisuru11@gmail.com>
2025-03-13 10:08:56 +0530

    Edited data

--- src/data/tutorials.ts
+++ src/data/tutorials.ts
@@ -4,8 +4,8 @@ export const tutorials: Tutorial[] = [
   {
     id: "kubernetes-crash-course",
     title: "Kubernetes Crash Course",
-    channel: "TechWorld with Nana",
-    category: "devops",
+    channel: "KodeKloud",
+    category: "Devops",
     image:
       "https://i.ytimg.com/vi/XuSQU5Grv1g/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA_x1aKpN8fgt4HSP79ZMad5h0RZg",
     url: "https://www.youtube.com/watch?v=XuSQU5Grv1g",
